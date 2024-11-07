26b5d2167c2a6aeb59c4975301f099d0019a5cc5
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2024-11-08 00:46:32 +0100

    update 2 forgot to version files

--- characterbuilder/src/app/app.routes.ts
+++ characterbuilder/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import {Routes} from '@angular/router';
 import {CharacterBuilderComponent} from './character-builder/character-builder.component';
 
 export const routes: Routes = [
-  {path: '', pathMatch: "full", redirectTo: ''},
+  {path: '', pathMatch: "full", redirectTo: 'builder'},
   {path: 'builder', component: CharacterBuilderComponent}
 ];
--- characterbuilder/src/app/character-api.service.ts
+++ characterbuilder/src/app/character-api.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {firstValueFrom} from 'rxjs';
+
+ type CharacterOption = {
+  eye: string,
+  hasHammer: boolean,
+  mouth: string,
+  rightHand: string,
+  hasTail: boolean
+}
+
+export type URL = {
+   url: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterAPIService {
 
-  constructor() { }
+  private httpClient = inject(HttpClient);
+  private readonly BASE_URL: string = "http://localhost:5110/"
+
+  getCharacterImageURL(characterOption: CharacterOption) {
+    return firstValueFrom(this.httpClient.post<URL>(this.BASE_URL + "build-image-url", characterOption));
+  }
+
+  getRandomImageOption(){
+    return firstValueFrom(this.httpClient.get<CharacterOption>(this.BASE_URL + "get-random-image-options"));
+  }
 }
